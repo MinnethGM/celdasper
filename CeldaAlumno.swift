5b35c1ff81461432006eeac70906c92df0b7caf1
Alumno <ulsa@iMacD112.local>
2018-09-18 18:56:45 -0700

    camb

---
+++ CeldaAlumno.swift
@@ -0,0 +1,16 @@
+//
+//  CeldaAlumno.swift
+//  CeldasPerzonalizadas
+//
+//  Created by Alumno on 18/09/18.
+//  Copyright © 2018 Alumno. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class CeldaAlumno: UITableViewCell {
+    
+    @IBOutlet weak var lblAlumno: UILabel!
+    
+
+}
